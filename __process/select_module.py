b1d21b3ded960714d2fd4c8fa653734a1108a8fc
Leni <sigld1004@naver.com>
2018-06-05 20:49:49 +0900

    select_module.py / error 동작 수정

--- __process/select_module.py
+++ __process/select_module.py
@@ -42,7 +42,6 @@ class Handler:
                         # 원래는 에이브릴을 생성해서 넣어주었다.
                         self.connection_list.append(client)
                         print("{} >> new client {} connected".format(time.ctime(), client_ip))
-                        print("client ip type >>".format(type(client_ip)))
 
                         # 사용자 ip를 기준으로 폴더를 생성하는 부분
                         # 이부분도 함수화 해야하지 않을까 싶다.
@@ -53,7 +52,6 @@ class Handler:
                     else:
                         audio = None
                         audios = []
-                        command = None
 
                         # < If select find old client >
                         # 실제는 이하 부분을 thread 로 돌리던지 해야 정확하게 비동기가 진행될 것 같다.
@@ -69,11 +67,13 @@ class Handler:
                         try:
                             # 음성 파일을 다운 받은 뒤에 stt, aibril 를 다녀와야 하기 때문에
                             # default 동작을 하는 함수를 불러와야 한다.
-                            command, text, language = mubby.default(input_audio_path)
+                            print("input_audio_path >> {} ".format(input_audio_path))
+                            header, text, language = mubby.default(input_audio_path)
                         except Exception as e:
                             print('\t★ default function error >> {}'.format(e))
 
                         try:
+                            print("header >> {}\ntext >> {}\nlanguage >> {}".format(header, text, language))
                             # < What should the server do? >
                             # Aibril 의 header.command 를 이용하여 어떤 동작을 할 것인지 정한다.
                             # 함수명 동작을 하는 함수를 불러와야 한다.
@@ -81,15 +81,15 @@ class Handler:
                             # return wave_file_path
 
                             # 하위 부분( - - - 까지) mubby_function 에 기능부분으로 만들어서 넣고 호출만 할 것.
-                            if command == "chat":
+                            if header['command'] == "chat":
                                 # tts 에 다녀온다.
                                 output_audio_path = mubby.chat_func(sock, text)
 
-                            elif command == "weather":
+                            elif header['command'] == "weather":
                                 # aibril 과 대화를 한 번 더 하고 tts 에 다녀온다.
                                 output_audio_path = mubby.weather_func()
 
-                            elif command == "music":
+                            elif header['command'] == "music":
                                 # (현재)
                                 # tts 에 다녀오고
                                 # 파일을 합치고 나서 반환한다.
@@ -105,10 +105,15 @@ class Handler:
 
                         try:
                             # < The server sent data to client >
-                            # 음성 파일을 돌려주어야 한다.
+                            # 음성 파일을 돌려주어야 한다. (확인)
+                            # output_audio_path = "__user_audio/"+sock.getpeername()[0] + "/output_tts.wav"
+                            print("out_audio_path >> {}".format(output_audio_path))
                             mubby.server.sending_wav(sock, output_audio_path)
                         except Exception as e:
                             print('\t★ __send function error >> {}'.format(e))
+
+                        # (테스트용 모피) 한 동작 후 연결을 끊기 때문에 소켓 종료가 들어가야 서버가 여러번 정상동작을 할 수 있다.
+                        mubby.server.close(sock)
 
             except Exception as e:
                 print('\t★ Program error >> {}'.format(e))
