c07609b4fa9b5d9c6eb20b7ee6d0412ee56cfc58
Leni <sigld1004@naver.com>
2018-06-05 18:00:55 +0900

    select_module.py
    세부 기능 호출 부분 추가

--- __process/select_module.py
+++ __process/select_module.py
@@ -5,12 +5,8 @@ import select
 import time
 # file 및 dir 존재 유무를 판단하기 위해서 사용
 import os
-
-# 이것도 Utils.__socket 안에다가 넣어야함.
-HOST = ''
-PORT = 5555
-BUFSIZE = 1024
-ADDR = (HOST, PORT)
+# 무삐의 기본 동작에 필요한 함수들이 다 들어가 있다.
+import __utils.mubby_processor as mubby
 
 
 class SocketProcess(multiprocessing.Process):
@@ -19,21 +15,22 @@ class SocketProcess(multiprocessing.Process):
         self.socket_process = None
 
     def run(self):
-        self.socket_process = Socket()
+        self.socket_process = Handler()
         self.socket_process.handler()
 
 
-class Socket:
+class Handler:
     def __init__(self):
-        self.connection_list = []
-        # 소켓 정의 및 커넥션 하는 함수 호출
-        # 소켓 생성 후 리턴해서 self.connection_list 에 넣어주어야 한다.
-        pass
+        self.connection_list = [mubby.server.getting_server()]
+
+        print(" SERVER is running {}".format('-'*10))
+
+        # 에이브릴 커넥터, 구글 등 객체 생성은 여기서 하도록 한다.
 
     def handler(self):
         while self.connection_list:
             # 로그로 수정해야하는 부분
-            print("=")
+            print("-")
             try:
                 read_sock, write_sock, error_sock = select.select(self.connection_list, [], [], 5)
                 # 이 경우를 실패했을 때는 어떻게 해야하지? 오류를 반환해야하나? 프로그램을 새로 시작해야 하나?
@@ -48,30 +45,31 @@ class Socket:
                         print("client ip type >>".format(type(client_ip)))
 
                         # 사용자 ip를 기준으로 폴더를 생성하는 부분
-                        if not os.path.exists('User_audio/'+client_ip[0]):
+                        # 이부분도 함수화 해야하지 않을까 싶다.
+                        if not os.path.exists('__user_audio/'+client_ip[0]):
                             print('\tmake {} dir'.format(client_ip[0]))
-                            os.system('mkdir User_audio/'+client_ip[0])
+                            os.system('mkdir __user_audio/'+client_ip[0])
 
                     else:
+                        audio = None
+                        audios = []
+                        command = None
+
                         # < If select find old client >
                         # 실제는 이하 부분을 thread 로 돌리던지 해야 정확하게 비동기가 진행될 것 같다.
                         # 각 try 별로 isSuccess 를 달아서 실패하면 더 이상 동작하지 않게 해야할 것 같다.
+                        # 이하 내용을 client = 1 class 형식으로 담아서 주고 받아야 하지 않을까 싶다.
 
                         try:
                             # < The server received data from client >
-                            # def __recv(file_path, sock)
-                            # return data
-                            pass
+                            input_audio_path = mubby.pcm2wav(sock)
                         except Exception as e:
                             print('\t★ default function error >> {}'.format(e))
 
                         try:
                             # 음성 파일을 다운 받은 뒤에 stt, aibril 를 다녀와야 하기 때문에
                             # default 동작을 하는 함수를 불러와야 한다.
-                            # def default(file_path, data)
-                            # return text_file
-                            command, text, language = "command", "text", "language"
-                            pass
+                            command, text, language = mubby.default(input_audio_path)
                         except Exception as e:
                             print('\t★ default function error >> {}'.format(e))
 
@@ -85,26 +83,30 @@ class Socket:
                             # 하위 부분( - - - 까지) mubby_function 에 기능부분으로 만들어서 넣고 호출만 할 것.
                             if command == "chat":
                                 # tts 에 다녀온다.
-                                pass
+                                output_audio_path = mubby.chat_func(sock, text)
+
                             elif command == "weather":
                                 # aibril 과 대화를 한 번 더 하고 tts 에 다녀온다.
-                                pass
+                                output_audio_path = mubby.weather_func()
+
                             elif command == "music":
                                 # (현재)
                                 # tts 에 다녀오고
                                 # 파일을 합치고 나서 반환한다.
                                 # ps. 클라이언트와 상의해보고 동작방법을 바꿔야 할 수도 있다.
-                                pass
+                                output_audio_path = mubby.music_func()
                             # - - -
-                            pass
+                            else:
+                                print("기본 음성 들어가게 해야하는데ㅔㅔㅔㅔㅔ")
+                                # 잘못된 접근이라는 것을 알려주어야 한다.
+
                         except Exception as e:
                             print('\t★ 함수명 function error >> {}'.format(e))
 
                         try:
                             # < The server sent data to client >
                             # 음성 파일을 돌려주어야 한다.
-                            # def __send(wave_file_path, sock)
-                            pass
+                            mubby.server.sending_wav(sock, output_audio_path)
                         except Exception as e:
                             print('\t★ __send function error >> {}'.format(e))
 
