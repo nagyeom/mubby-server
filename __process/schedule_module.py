906bebd87a7b552e5e9ef1faaa781a38d48559e0
Yonghan Kim <nearkyh@naver.com>
2018-06-07 14:16:36 +0900

    Update schedule_module.py
    Add socket processing.

--- __process/schedule_module.py
+++ __process/schedule_module.py
@@ -1,27 +1,57 @@
 import threading
 import sys
+import socket
 
+from datetime import datetime
 from __utils import mysql_connector
 
 
-sys.setrecursionlimit(1500)
-
 class Scheduling:
     def __init__(self):
-        self.tbl_message()
-        self.tbl_mubby()
+        # self.srl = sys.setrecursionlimit(10**6)
+        self.ms_conn = mysql_connector.MubbyQuery()
+        self.current_time()
+        # self.task_tbl_mubby()
+        self.task_tbl_message()
 
-    def tbl_mubby(self):
-        rows = mysql_connector.MubbyQuery().select_tbl_mubby()
+        self.HOST = ''
+        self.PORT = 50000
+        self.ADDR = (self.HOST, self.PORT)
+        self.BUFF_SIZE = 1024
+
+    def connect_socket(self):
+        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        clientSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        clientSocket.connect(self.ADDR)
+        return clientSocket
+
+    def current_time(self):
+        now = datetime.now()
+        return "%04d-%02d-%02d %02d:%02d:00" % (now.year, now.month, now.day, now.hour, now.minute)
+
+    def task_tbl_mubby(self):
+        rows = self.ms_conn.select_tbl_mubby()
         for row in rows:
-            print(row)
-        threading.Timer(5, self.tbl_mubby).start()
+            print("사용자 정보 테이블>>", row)
+        threading.Timer(5, self.task_tbl_mubby).start()
 
-    def tbl_message(self):
-        rows = mysql_connector.MubbyQuery().select_tbl_message()
+    def task_tbl_message(self):
+        rows = self.ms_conn.select_tbl_message()
         for row in rows:
-            print(row)
-        threading.Timer(5, self.tbl_message).start()
+            print("사용자 메시지 정보>>", row)
+            try:
+                if str(row[3])==self.current_time():
+                    clientSocket = self.connect_socket()
+                    header = 'message'
+                    clientSocket.send(header.encode())
+                    # ack
+                    clientSocket.recv(self.BUFF_SIZE)
+                    # send mesage
+                    clientSocket.send(row[1].encode())
+                    # ack
+                    clientSocket.recv(self.BUFF_SIZE)
+            except Exception as e:
+                print(e)
 
-
+        threading.Timer(5, self.task_tbl_message).start()
 
