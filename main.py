485a4fb8b0729a83cb94608d4596c6b9d2bd104a
Leni <sigld1004@naver.com>
2018-06-08 17:00:58 +0900

    Time check

--- main.py
+++ main.py
@@ -43,92 +43,66 @@ def pcm2wav(path):
 
 
 def handler(clientSocket, addr, communi):
-    start = time.time()
+    print("Connected from", addr)
 
-    # print("Connected from", addr)
-    # buf = clientSocket.recv(1024)
-    # print('data > {}'.format(buf))
     f = open('record', 'wb')
-
     # print('ST_PROTO_RECORD_DATA')
-    while True:
+    buf = clientSocket.recv(1024)
+    print("buf {} ".format(buf))
 
-        buf = clientSocket.recv(1024)
-        # print('data > {}'.format(buf))
-        # if buf == b'end':
-        #     print('ST_PROTO_RECORD_STOP')
-        if buf[-3:] == b'end':
-            # print('ST_PROTO_RECORD_STOP')
-            f.write(buf[:-3])
-            f.close()
-            pcm2wav('record')
-            os.unlink('record')
-            # print('Streaming end.', addr)
-            break
-        f.write(buf)
+    # << File receiving >>
+    if buf == b'rec':
+        start = time.time()
+        while True:
+            buf = clientSocket.recv(1024)
+            if buf[-3:] == b'end':
+                # print('ST_PROTO_RECORD_STOP')
+                f.write(buf[:-3])
+                f.close()
+                file_recv_time = time.time() - start
 
-    file_recv_time = time.time() - start
-    start = time.time()
+                # << Pcm to Wave Converter >>
+                start = time.time()
+                pcm2wav('record')
+                os.unlink('record')
+                pcm_to_wav_time = time.time() - start
+                break
+            f.write(buf)
 
-    RECV_FILE = "record.wav"
-    result_audio_stt = stt_conn.audio_stt(RECV_FILE)
-    stt_time = time.time()-start
-    start = time.time()
+        # << google STT >>
+        RECV_FILE = "record.wav"
+        start = time.time()
+        result_audio_stt = stt_conn.audio_stt(RECV_FILE)
+        stt_time = time.time()-start
 
-    result_conversation = aibril_conn.aibril_conv(result_audio_stt)
-    aibril_time = time.time()-start
-    start = time.time()
+        # << Aibril conversation >>
+        start = time.time()
+        result_conversation = aibril_conn.aibril_conv(result_audio_stt)
+        aibril_time = time.time()-start
 
-    # ===========================================================
-    tts_conn.aws_tts(result_conversation)
-    aws_tts_time = time.time()-start
-    start = time.time()
+        # << awsPolly TTS >>
+        start = time.time()
+        tts_conn.aws_tts(result_conversation)
+        aws_tts_time = time.time()-start
 
-    SEND_FILE = audio_converter.convert("output_atts.mp3")
-    convert_time = time.time()-start
-    strat = time.time()
+        # << mp3 to Wave Converter >>
+        start = time.time()
+        SEND_FILE = audio_converter.convert("output_atts.mp3")
+        convert_time = time.time()-start
 
-    with open(SEND_FILE, 'rb') as f:
-        data = f.read()
-    # print("size >> {}".format(len(data)))
-    clientSocket.send(str(len(data)).encode())
-    a = clientSocket.recv(1024)
-    # print("recv{}".format(a))
-    communi.sending_wav(clientSocket, SEND_FILE)
-    #
-    # print('send file name > {}'.format(SEND_FILE))# "output_tts.wav"
-    #
-    # # SEND_FILE = "output_tts.wav"
-    #
-    # print('Completed')
-    #
-    # wave.set_sock(clientSocket, SEND_FILE)
-    # wave.include_header()
-    what = clientSocket.recv(1024)
-    file_send_time = time.time()-start
-    # print("what? {} ".format(what))
-    clientSocket.close()
-    #
-    # while True:
-    #     print('\nCHOICE{}\n\t- 1. without_header\n\t- 2. include_header\n\t- 0. exit'.format('- ' * 10))
-    #     inp = input('>> ')
-    #
-    #     if inp in ('1', '2'):
-    #         wave.set_sock(clientSocket, SEND_FILE)
-    #         if inp == '1':
-    #             wave.without_header()
-    #         elif inp == '2':
-    #             wave.include_header()
-    #         clientSocket.close()
-    #         break
-    #     elif inp == '0':
-    #         print('STOP PROGRAM')
-    #         clientSocket.close()
-    #         break
-    #     else:
-    #         print('- - - plz, choice 1 or 2\n')
+        start = time.time()
+        with open(SEND_FILE, 'rb') as f:
+            data = f.read()
+        # print("size >> {}".format(len(data)))
+        clientSocket.send(str(len(data)).encode())
+        a = clientSocket.recv(1024)
+        # print("recv{}".format(a))
+        communi.sending_wav(clientSocket, SEND_FILE)
+        what = clientSocket.recv(1024)
+        file_send_time = time.time()-start
+        clientSocket.close()
 
-    return {"file_recv_time": file_recv_time, "stt_time": stt_time, "aibril_time": aibril_time, "aws_tts_time": aws_tts_time, "convert_time": convert_time, "file_send_time": file_send_time}
+        return {"file_recv_time": file_recv_time, "pcm_to_wav_time": pcm_to_wav_time, "stt_time": stt_time, "aibril_time": aibril_time, "aws_tts_time": aws_tts_time, "convert_time": convert_time, "file_send_time": file_send_time}
 
 if __name__ == '__main__':
     while True:
