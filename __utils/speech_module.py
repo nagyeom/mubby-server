0bd247bfb355e772b27d0864fee22c0123a3617e
Leni <sigld1004@naver.com>
2018-07-12 14:32:05 +0900

    < MODIFY > STT Streaming is been Updating

--- __utils/speech_module.py
+++ __utils/speech_module.py
@@ -42,10 +42,90 @@ class SpeechToText:
 
         return output_stt
 
-    def google_stt_streaming(self, client_info):
-        # 동작 가져와아아아아아아아아아아아아ㅏ하
-        # 소켓 정보도 받아와야 한다 음하하하하
-        pass
+    def listen_print_loop(self, responses):
+        """
+            Iterates through server responses and prints them.
+
+            The responses passed is a generator that will block until a response
+            is provided by the server.
+
+            Each response may contain multiple results, and each result may contain
+            multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+            print only the transcription for the top alternative of the top result.
+
+            In this case, responses are provided for interim results as well. If the
+            response is an interim one, print a line feed at the end of it, to allow
+            the next result to overwrite it, until the response is a final one. For the
+            final one, print a newline to preserve the finalized transcription.
+        """
+        text = ''
+        num_chars_printed = 0
+        for response in responses:
+            if not response.results:
+                continue
+
+            # The `results` list is consecutive. For streaming, we only care about
+            # the first result being considered, since once it's `is_final`, it
+            # moves on to considering the next utterance.
+            result = response.results[0]
+            if not result.alternatives:
+                continue
+
+            # Display the transcription of the top alternative.
+            transcript = result.alternatives[0].transcript
+
+            # Display interim results, but with a carriage return at the end of the
+            # line, so subsequent lines will overwrite them.
+            #
+            # If the previous result was longer than this one, we need to print
+            # some extra spaces to overwrite the previous result
+            overwrite_chars = ' ' * (num_chars_printed - len(transcript))
+
+            if not result.is_final:
+                sys.stdout.write(transcript + overwrite_chars + '\r')
+                sys.stdout.flush()
+
+                num_chars_printed = len(transcript)
+
+            else:
+                # print(transcript + overwrite_chars)
+                text = transcript + overwrite_chars
+
+                # Exit recognition if any of the transcribed phrases could be
+                # one of our keywords.
+                if re.search(r'\b(exit|quit)\b', transcript, re.I):
+                    print('Exiting..')
+                    break
+
+                num_chars_printed = 0
+
+        return text
+
+    def google_stt_streaming(self, socket_action, client_info=None):
+        if client_info is not None:
+            # See http://g.co/cloud/speech/docs/languages
+            # for a list of supported languages.
+            language_code = 'ko-KR'  # a BCP-47 language tag
+
+            # for content in comuni.get_data(client_record):
+            #     print("Type >> {}".format(type(content)))
+
+            client = speech.SpeechClient()
+            config = types.RecognitionConfig(
+                encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
+                sample_rate_hertz=RATE,
+                language_code=language_code)
+            streaming_config = types.StreamingRecognitionConfig(
+                config=config,
+                interim_results=True)
+            requests = (types.StreamingRecognizeRequest(audio_content=content)
+                        for content in socket_action.get_data(client_record))
+
+            responses = client.streaming_recognize(streaming_config, requests)
+
+            # Now, put the transcription responses to use.
+            text = self.listen_print_loop(responses)
+            return text
 
 
 # 어떻게 동작하는지 확인할 것
